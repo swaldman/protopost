4816be3fd68f542e3dcebcdcebc478dcfe7aa5bc
Steve Waldman <swaldman@mchange.com>
2025-09-16 23:56:19 -0400

    Remove unused typewrapper.scala file.

--- src/protopost/server/main/typewrapper.scala
+++
@@ -1,9 +0,0 @@
-package protopost.server.main
-
-import java.util.Properties
-
-import scala.annotation.targetName
-
-// XXX: I'll keep this around in case there eventually are main-specific typewrappers again
-//      Dummy is to shut up warnings...
-val Dummy : Int = -1
